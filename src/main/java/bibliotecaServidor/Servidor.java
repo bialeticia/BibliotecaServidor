42d73b0690a7401c9b74a817159d1924493df971
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-06-16 16:20:36 -0300

    Add: Criando thread com ClienteHandler e método para carregar livros

--- src/main/java/bibliotecaServidor/Servidor.java
+++ src/main/java/bibliotecaServidor/Servidor.java
@@ -1,10 +1,33 @@
 package bibliotecaServidor;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Servidor {
-
+	private static final int PORT = 1234;
+	private static List<Livro> livros;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 try {
+	            livros = carregarLivros();
+	            ServerSocket serverSocket = new ServerSocket(PORT);
+	            System.out.println("Servidor iniciado na porta " + PORT);
 
+	            while (true) {
+	                Socket clientSocket = serverSocket.accept();
+	                new Thread(new ClienteHandler(clientSocket)).start();
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	}
-
+	
+	 private static List<Livro> carregarLivros() throws IOException {
+		 ObjectMapper mapper = new ObjectMapper();
+		 Biblioteca biblioteca = mapper.readValue(new File("src/main/resources/livros.json"), Biblioteca.class);
+		 return biblioteca.getLivros();
+	 }
 }
